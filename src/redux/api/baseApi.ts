0bd2b8465a8157732780320c4a87d5912d658fcb
sumonpaul55 <sumpaul100@gmail.com>
2024-09-17 17:38:57 +0600

    base api setted up for getting requ rtk query

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  endpoints: (builder) => ({
+    getBlogs: builder.query({
+      query: () => {
+        return {
+          url: `/blogs`,
+          method: "GET",
+        };
+      },
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetBlogsQuery } = baseApi;
